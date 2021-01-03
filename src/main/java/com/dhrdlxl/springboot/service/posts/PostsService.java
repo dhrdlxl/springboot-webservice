2c0dc0fd8ff12d01c5d13ef9736b2060b3ecb6cd
SeungHyunYoo <dhrdlxl@naver.com>
2021-01-03 20:21:43 +0900

    feat(findALlDesc): 전체 조회 기능 및 페이지 추가

--- src/main/java/com/dhrdlxl/springboot/service/posts/PostsService.java
+++ src/main/java/com/dhrdlxl/springboot/service/posts/PostsService.java
@@ -2,12 +2,16 @@ package com.dhrdlxl.springboot.service.posts;
 
 import com.dhrdlxl.springboot.domain.posts.Posts;
 import com.dhrdlxl.springboot.domain.posts.PostsRepository;
+import com.dhrdlxl.springboot.web.dto.PostsListResponseDto;
 import com.dhrdlxl.springboot.web.dto.PostsResponseDto;
 import com.dhrdlxl.springboot.web.dto.PostsSaveRequestDto;
 import com.dhrdlxl.springboot.web.dto.PostsUpdateRequestDto;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
+
+import java.util.List;
+import java.util.stream.Collectors;
 
 @RequiredArgsConstructor
 @Service
@@ -35,5 +39,12 @@ public class PostsService {
                 new IllegalArgumentException("해당 게시글이 없습니다. id =" + id));
 
         return new PostsResponseDto(entity);
+    }
+
+    @Transactional(readOnly = true)
+    public List<PostsListResponseDto> findAllDesc() {
+        return postsRepository.findAllDesc().stream()
+                .map(PostsListResponseDto::new)
+                .collect(Collectors.toList());
     }
 }
